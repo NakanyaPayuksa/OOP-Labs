853f49d2736091e4514af3830144661d5cb425ca
AkkarawitWanthit <6930613004@psu.ac.th>
2026-09-14 16:06:12 +0700

    add Akkarawit

---
+++ lab0/Akkarawit.java
@@ -0,0 +1,39 @@
+package lab0;
+
+public class Akkarawit {
+
+    public static void main(String[] args) {
+        main();
+        main();
+        main();
+        main();
+        main();
+        main();
+        main();
+        main();
+        main();
+        main();
+        main();
+        main();
+        main();
+        main();
+        main();
+        main();
+        main();
+        main();
+        main();
+        main();
+        main();
+        main();
+        main();
+        main();
+        main();
+        main();
+        main();
+        main();
+        main();
+    }
+    static void main(){
+        System.out.println("Hahahahhahahahhaha");
+    }
+}
