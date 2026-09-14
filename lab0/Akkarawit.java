3d06a5fb66df82651cadb412aa70eed9022821d8
AkkarawitWanthit <6930613004@psu.ac.th>
2026-09-14 16:11:51 +0700

    add Akkarawit

--- lab0/Akkarawit.java
+++ lab0/Akkarawit.java
@@ -1,39 +1,26 @@
 package lab0;
-
 public class Akkarawit {
-
     public static void main(String[] args) {
-        main();
-        main();
-        main();
-        main();
-        main();
-        main();
-        main();
-        main();
-        main();
-        main();
-        main();
-        main();
-        main();
-        main();
-        main();
-        main();
-        main();
-        main();
-        main();
-        main();
-        main();
-        main();
-        main();
-        main();
-        main();
-        main();
-        main();
-        main();
-        main();
+        int i;
+        for(i=0;i<5;i++){
+            main();
+        }
     }
     static void main(){
-        System.out.println("Hahahahhahahahhaha");
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 7; j++) {
+                if ((i == 0 && (j == 1 || j == 2 || j == 4 || j == 5)) ||
+                    (i == 1 && (j != 0 && j != 6)) ||
+                    (i == 2 && (j != 0 && j != 6)) ||
+                    (i == 3 && (j != 1 && j != 5)) ||
+                    (i == 4 && (j != 2 && j != 4)) ||
+                    (i == 5 && j == 3)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
