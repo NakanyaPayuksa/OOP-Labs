cd1f5197e4201f549d7f6eac43fa6f3ffd257f78
AkkarawitWanthit <6930613004@psu.ac.th>
2026-09-15 08:56:01 +0700

    add HelloWorld

--- lab0/HelloWorld.java
+++ lab0/HelloWorld.java
@@ -1,30 +1,42 @@
 package lab0;
-
 import java.util.Scanner;
-
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
         Scanner sc=new Scanner(System.in);
-        int i;
-        System.out.print("Enter :");
-        i = sc.nextInt();
-        for(int j=1;j<=5;j++){
-            main(i);
-
+        char check;
+        String comment;
+        System.out.print("Do you want to play ? (Y/N) : ");
+        check = sc.next().charAt(0);
+        if(check=='Y'||check=='y'){
+            System.out.println("Ready !!!");
+            main(sc);
+        }
+        else if(check=='N'||check=='n'){
+            System.out.println("Why don't you want to play? ");
+            System.out.print("Give me a reason why I don't want to play : ");
+            comment = sc.nextLine();
+        }
+        else{
+            System.out.println("ERROR!!!");
         }
     }
-    static void main (int x){
-        for(int i=1;i<=x;i++){
-            for (int j=1;j<=x;j++) {
-                if(i==x||i==1||j==1||j==x){
-                    System.out.print("*");
-                }
-                else{
-                    System.out.print(" ");
-                }
+    static void main (Scanner sc){
+        char check;
+        while (true) {
+            System.out.print("Do you love me ? (Y/N) : ");
+            check = sc.next().charAt(0);
+            if(check=='Y'||check=='y'){
+                System.out.println("I Love You Too My Love.");
+                break;
             }
-            System.out.println("");
+            else if(check=='N'||check=='n'){
+                System.out.println("I'm Very Sad. T_T");
+                break;
+            }
+            else{
+                System.out.println("ERROR!!!");
+            }    
         }
     }
 }
